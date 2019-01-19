6fd8febb9fe3a3c7df3c499c105545c9365d9f22
Rhett Del Fierro <thinquetank@gmail.com>
2019-01-19 07:52:33 -0800

    init

---
+++ src/components/Test.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+
+export const Test = () => {
+  const usersData = [
+    { id: 1, name: 'Tania', username: 'floppydiskette' },
+    { id: 2, name: 'Craig', username: 'siliconeidolon' },
+    { id: 3, name: 'Ben', username: 'benisphere' },
+  ]
+
+  const [ users, setUsers ] = useState(usersData)
+
+  return (
+    <div className="container">
+      <h1>CRUD App with Hooks</h1>
+      <div className="flex-row">
+        <div className="flex-large">
+          <h2>Add user</h2>
+        </div>
+        <div className="flex-large">
+          <h2>View users</h2>
+          {users.map(u => <h1>{u.name}</h1>)}
+        </div>
+      </div>
+    </div>
+  )
+}
