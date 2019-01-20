d9a46ca6d7e430ea39efeef803bfa2b1a989b79b
Rhett Del Fierro <thinquetank@gmail.com>
2019-01-20 07:21:27 -0800

    cleanup

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,70 +1,11 @@
-import React, {useEffect, useState, useRef} from 'react'
-import {Form, Icon, Input, Button} from 'antd';
+import React, {} from 'react'
 import '../../App.css';
-const {Item} = Form
+import BookForm from './Form'
 
 
-const Body = ({form}) => {
-  const {getFieldDecorator, getFieldValue} = form
+const Body = () =>
+  <>
+    <BookForm/>
+  </>
 
-  const handleSubmit = (event) => {
-    event.preventDefault()
-    form.validateFields((err, values) => {
-      if (!err) {
-        console.log('received values of form: ', values)
-      }
-    })
-  }
-
-  const handleAuthorEmpty = () => { form.resetFields(['author']) }
-  const handleTitleEmpty = () => { form.resetFields(['title']) }
-
-
-  const autoFocusInt = useRef()
-  useEffect(() => {
-    autoFocusInt.current.focus()
-  }, [])
-
-  const checkValue = (rule, value, callback) => {
-    const {author, title} = form.getFieldsValue()
-    console.log(author, title)
-    if (!author && !title)
-        return callback('At least one input required.')
-    else callback()
-  }
-
-  const authorSuffix = getFieldValue('author') ? <Icon type="close-circle" onClick={handleAuthorEmpty}/> : null;
-  const titleSuffix = getFieldValue('title') ? <Icon type="close-circle" onClick={handleTitleEmpty}/> : null;
-
-  return (
-    <Form onSubmit={handleSubmit} className="login-form">
-      <Item>
-        {getFieldDecorator('author', {rules: [ {validator: checkValue} ]})
-        (
-          <Input
-            placeholder="Enter an author"
-            prefix={<Icon type="user" style={{color: 'rgba(0,0,0,.25)'}}/>}
-            suffix={authorSuffix}
-            ref={autoFocusInt}
-          />
-        )}
-      </Item>
-      <Item>
-        {getFieldDecorator('title', {rules: [{validator: checkValue}]})
-        (
-          <Input
-            placeholder="Enter a title"
-            prefix={<Icon type="book" style={{color: 'rgba(0,0,0,.25)'}}/>}
-            suffix={titleSuffix}
-            ref={autoFocusInt}
-          />
-        )}
-      </Item>
-      <Item>
-        <Button type="primary" htmlType="submit">Submit</Button>
-      </Item>
-    </Form>
-  )
-}
-
-export default Form.create()(Body)
+export default Body
